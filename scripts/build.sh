555991f7ee0fece82400a255f454d9fdd48d4ba4
Yehiyam Livneh <yehiyam@gmail.com>
2022-02-13 15:48:47 +0200

    update pkg version

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 PLATFORM=${1:-linux}
-NODE_VER=${2:-node10}
+NODE_VER=${2:-node16}
 echo building ${PLATFORM} platform for ${NODE_VER}
 cp "${DIR}/../platform/syncthing-${PLATFORM}" "${DIR}/../helpers/syncthing/syncthing"
 "${DIR}/../node_modules/.bin/pkg" --options no-warnings -t ${NODE_VER}-${PLATFORM} -o "${DIR}/../output/hkubectl-${PLATFORM}" "${DIR}/../" 
