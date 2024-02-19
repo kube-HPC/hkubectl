39a628a13cd025d607cc0e05271a0652e13b8396
ronshv <lepilmen@gmail.com>
2024-02-19 17:35:51 +0200

    Added sync start

--- commands/sync/index.js
+++ commands/sync/index.js
@@ -1,7 +1,9 @@
 const watch = require('./watch');
 const create = require('./create');
+const start = require('./start');
 
 module.exports = {
     watch,
-    create
+    create,
+    start
 };
---
+++ commands/sync/start.js
@@ -0,0 +1,66 @@
+const { post } = require('../../helpers/request-helper');
+
+const startHandler = async ({ endpoint, rejectUnauthorized, algorithmName, devFolder, $0: appName }) => {
+    // get all parameters, decorate,
+    const body = {
+        name: algorithmName,
+        options: {
+            devMode: true,
+            devFolder
+        }
+    };
+    // send update, if doesn't exist, exit.
+    const res = await post({ endpoint, rejectUnauthorized, path: 'store/algorithms?overwrite=true', body });
+    if (res.result.error || res.error) {
+        let msg;
+        if (res.result.error.code === 400) {
+            msg = "algorithm doesn't exist";
+        }
+        else {
+            msg = res.result.error.message;
+        }
+        console.log(`code: ${res.result.error.code}, message: ${msg}`);
+        return;
+    }
+    console.log(`algorithm ${algorithmName} modified to be in development mode.`);
+    console.log('to sync the folder to the algorithm run');
+    console.log(`${appName} sync watch -a ${algorithmName} -f localAlgoFolder}`);
+};
+module.exports = {
+    command: 'start',
+    description: 'start an existing algorithm in development mode',
+    options: {
+    },
+    builder: {
+        algorithmName: {
+            demandOption: true,
+            describe: 'The name of the algorithm to sync data into',
+            type: 'string',
+            alias: ['a']
+        },
+        devFolder: {
+            demandOption: true,
+            describe: 'folder in pod to sync to',
+            type: 'string',
+            alias: ['f']
+        },
+        env: {
+            describe: 'algorithm runtime environment',
+            type: 'string',
+            choices: ['python', 'nodejs']
+
+        },
+        entryPoint: {
+            describe: 'the main file of the algorithm',
+            type: 'string',
+            alias: ['e']
+        },
+        baseImage: {
+            describe: 'base image for the algorithm',
+            type: 'string'
+        }
+    },
+    handler: async (argv) => {
+        await startHandler(argv);
+    }
+};
