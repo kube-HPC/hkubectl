297ef81685b385c8f5a4dd4c421c3b746979c7aa
ronshv <lepilmen@gmail.com>
2024-03-11 10:36:37 +0200

    Added menu with scrollingLine to console, to handle "apply sync", Added exit mechanism form tunnel stream, Mask the "premature close" error output since it doesn't stop the sync process, Version upgrade api placeholder

--- commands/sync/watch.js
+++ commands/sync/watch.js
@@ -1,11 +1,65 @@
 const path = require('path');
 const getPort = require('get-port');
-const socketTunnelClient = require('../../helpers/tcpTunnel/client');
+const readline = require('readline');
+const { startClient, releaseTunnel } = require('../../helpers/tcpTunnel/client');
 const agentSyncIngressPath = '/hkube/sync/sync';
 const agentRestIngressPath = '/hkube/sync/ui';
 const syncthing = require('../../helpers/syncthing/syncthing.js');
 const { events } = require('../../helpers/consts');
 const { lock } = require('../../helpers/locks');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const _finishedUpdate = () => {
+    releaseTunnel();
+    console.log('finishing update process, releasing tunnel client and exiting.');
+};
+
+const printScrollingLine = (actionLog, addSelectMsg = true) => {
+    const timestamp = new Date().toLocaleString();
+    let logMessage = `[${timestamp}] ${actionLog}`;
+    if (addSelectMsg) {
+        logMessage = `${logMessage}         Please select an option:   `;
+    }
+    process.stdout.write('\x1b[1A\x1b[2K'); // Move cursor up one line and clear it
+    process.stdout.write(`${logMessage}\n`);
+};
+
+const startMenu = (printMenu = false) => {
+    if (printMenu) {
+        console.log('===========================================================');
+        console.log('\nWelcome to the sync watch control menu:');
+        console.log('1. Resync the folder');
+        console.log('2. Exit');
+        console.log('Please select an option:   ');
+    }
+
+    rl.question('', (answer) => {
+        switch (answer) {
+        case '1':
+            process.stdout.write('\x1b[1A\x1b[2K'); // Move cursor up one line and clear it
+            printScrollingLine('Pressed 1, restarting pods, update in progress...', true);
+            // version upgrade api here //
+            startMenu(true);
+            break;
+        case '2':
+            process.stdout.write('\x1b[1A\x1b[2K'); // Move cursor up one line and clear it
+            printScrollingLine('Exiting...');
+            syncthing._proc.kill();
+            _finishedUpdate();
+            break;
+        default:
+            process.stdout.write('\x1b[1A\x1b[2K'); // Move cursor up one line and clear it
+            printScrollingLine('Invalid option. Please try again. ');
+            // eslint-disable-next-line no-unused-vars
+            startMenu();
+            break;
+        }
+    });
+};
 
 const watchHandler = async ({ endpoint, rejectUnauthorized, algorithmName, folder, bidi }) => {
     const tunnelUrl = `${endpoint}/${agentSyncIngressPath}`.replace('http', 'ws');
@@ -16,7 +70,7 @@ const watchHandler = async ({ endpoint, rejectUnauthorized, algorithmName, folde
         const unlock = await lock('tunnelPort');
         try {
             tunnelPort = await getPort({ port: 22001 });
-            await socketTunnelClient(tunnelUrl, 'localhost:22000', tunnelPort, { rejectUnauthorized });
+            await startClient(tunnelUrl, 'localhost:22000', tunnelPort, { rejectUnauthorized });
         }
         finally {
             unlock();
@@ -30,23 +84,25 @@ const watchHandler = async ({ endpoint, rejectUnauthorized, algorithmName, folde
             }
             switch (data.type) {
             case events.FolderSummary:
-                console.log(`[${data.name}] Algorithm ${data.folder} update started`);
+                printScrollingLine(`[${data.name}] Algorithm ${data.folder} update started `);
                 break;
             case events.FolderCompletion:
-                console.log(`[${data.name}] Algorithm ${data.folder} update done`);
+                printScrollingLine(`[${data.name}] Algorithm ${data.folder} update done `);
                 break;
             default:
                 break;
             }
         });
+        startMenu(true);
     }
     catch (error) {
         console.error(`error connecting sync server. Error: ${error.message}`);
     }
 };
+
 module.exports = {
     command: 'watch',
-    description: 'watch a local folder',
+    description: 'watch a local folder, navigate menu to apply changes instantly',
     options: {
     },
     builder: {
